ff8d9316273459490057347b71d32ea3c3901fbb
HUDRSON DE OLIVEIRA SILVA <hudrson.silva@alegre.es.gov.br>
2025-06-23 14:02:45 -0300

    fase 0

---
+++ gestao-servicos-api/src/main/java/br/com/hstecno/gestaoservicos/GestaoServicosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hstecno.gestaoservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoServicosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoServicosApiApplication.class, args);
+	}
+
+}
---
+++ gestao-servicos-api/src/test/java/br/com/hstecno/gestaoservicos/GestaoServicosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hstecno.gestaoservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoServicosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
